8776c13184e6a44d8da10aef4dcd670ba3b3d1f2
VitaPasser <vitapasser@gmail.com>
2024-11-05 21:17:37 +0200

    Прослушивание и адрес для страницы

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import express from "express";
 const app = express();
+const port = 3000
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+app.listen(port, () => {
+  console.log(`App started and listening on port 3000\n Visit page: http://localhost:${port}`)
+})
