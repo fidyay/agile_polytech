1e641c7ff835aa38952fa638d7d592c91fef792e
Fidyay <fidiy1878@gmail.com>
2024-11-05 20:57:16 +0200

    dfsf

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world");
+import express from "express";
+const app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
